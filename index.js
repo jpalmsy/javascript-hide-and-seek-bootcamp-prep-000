57208669995be0d4b3ad4cafacfa1e1313e89812
Joey Palmieri <joeypalmieri@hotmail.com>
2018-07-06 23:58:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function nestedTarget() {
 }
 
 function deepestChild() {
-  let node = document.getElementbyID('grand-node')
+  let node = document.getElementbyId('grand-node')
   let nextNode = node.children[0]
   while(nextNode) {
   node = nextnode
