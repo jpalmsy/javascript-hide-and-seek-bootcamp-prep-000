64f7072bb257dc9eae2d59ac853f539454f6c819
Joey Palmieri <joeypalmieri@hotmail.com>
2018-07-07 00:10:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function increaseRankBy() {
   const rankslist = document.querySelector('.ranked-list')
   
   for (let i = 0, l = rankslist.length; i < l; i++) {
-    let childen = rankslist[i] 
+    let childen = rankslist[i].children 
   
   for (j = 0, k = children.length; j < k; j++) {
   children[j].innerhtml = parseInt(children[j.innerhtml]) + n
